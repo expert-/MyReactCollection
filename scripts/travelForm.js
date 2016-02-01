3af001b4fbe7f427cc30ac4dc1c3594213dd478a
Ayush <ayush15may@gmail.com>
2016-02-01 19:57:57 +0530

    Parent component created as TotalTraveller

--- scripts/travelForm.js
+++ scripts/travelForm.js
@@ -2,7 +2,7 @@
  * Created by Ayush on 1/30/2016.
  */
 
-var APP = React.createClass({
+var PersonCount = React.createClass({
     getInitialState: function () {
         return {
             count: 0
@@ -14,6 +14,9 @@ var APP = React.createClass({
         }) : this.setState({
             count: this.state.count
         });
+        this.state.count < 9 ? this.props.update1() : this.setState({
+            count: this.state.count
+        });
     },
     down: function () {
         this.state.count > 0 ? this.setState({
@@ -21,26 +24,66 @@ var APP = React.createClass({
         }) : this.setState({
             count: this.state.count
         });
+        this.state.count > 0 ? this.props.update2() : this.setState({
+            count: this.state.count
+        });
+
     },
     render: function () {
         return (
-            <div>
+            <div className="numOfAdults">
                 <p id="adults">{this.props.text}</p>
-                    <div id="nop">
-                        <button type="button" className="btn" onClick={this.down}>
-                            <span className="glyphicon glyphicon-minus"></span>
-                        </button>
-                        <label id="nopText" maxLength="1" min="0" max="10"
-                               value={this.state.count >=0 ? this.state.count : '0'}
-                               className={this.state.count>0 ? 'green' : 'red'}>{this.state.count}</label>
+                <div id="nop">
+                    <button type="button" className="btn" onClick={this.down}>
+                        <span className="glyphicon glyphicon-minus"></span>
+                    </button>
+                    <label id="nopText" maxLength="1" min="0" max="10"
+                           value={this.state.count >=0 ? this.state.count : '0'}
+                           className={this.state.count>0 ? 'green' : 'red'}>{this.state.count}</label>
 
-                        <button type="button" className="btn" onClick={this.up}>
-                            <span className="glyphicon glyphicon-plus"></span>
-                        </button>
+                    <button type="button" className="btn" onClick={this.up}>
+                        <span className="glyphicon glyphicon-plus"></span>
+                    </button>
                 </div>
             </div>
         )
     }
 });
-React.render(<APP text="Adult"/>, document.getElementById("numOfAdults"))
-React.render(<APP text="Children"/>, document.getElementById("children"))
+//React.render(<PersonCount text="Adult"/>, document.getElementById("numOfAdults"))
+//React.render(<PersonCount text="Children"/>, document.getElementById("children"))
+
+
+var TotalTraveller = React.createClass({
+    getInitialState: function () {
+        return {
+            totalPerson: 0
+        }
+    },
+    update1: function () {
+        this.state.totalPerson < 18 ? this.setState({
+            totalPerson: this.state.totalPerson + 1
+        }) : this.setState({
+            totalPerson: this.state.totalPerson
+        });
+    },
+    update2: function () {
+        this.state.totalPerson > 0 ? this.setState({
+            totalPerson: this.state.totalPerson - 1
+        }) : this.setState({
+            totalPerson: this.state.totalPerson
+        });
+    },
+    render: function () {
+        return (
+            <div>
+                    <PersonCount text="Adult" update1={this.update1} update2={this.update2}/>
+
+
+                    <PersonCount id="children" text="Children" update1={this.update1} update2={this.update2}/>
+
+                <div id="totalTravellerDisplay"><span>Total Travellers :</span>{this.state.totalPerson}</div>
+            </div>
+        )
+    }
+});
+React.render(<TotalTraveller />, document.getElementById("personCount"))
